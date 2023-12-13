9e6267cddb74b39957220c3bff7e2d3c0dea0764
Luthfikhaerii <luthfikhaeri00@gmail.com>
2023-12-13 20:42:25 +0700

    ,

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <>
       <CarouselCustom/>
+      <div className='p-14'>
+        <h1 className='text-center text-3xl font-bold'>LAB KIMIA UPI</h1>
+        <p className='text-center mt-14 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda sint qui libero neque natus veritatis delectus in praesentium vero saepe commodi impedit quis aspernatur, similique voluptas totam repellat fugiat ullam!</p>
+        
+      </div>
     </>
   )
 }
