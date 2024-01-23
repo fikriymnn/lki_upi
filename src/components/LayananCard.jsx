a1ca7e15ecffe62ad4a1b6ee787b07165d6ace53
Tio Noviar <thenoviar@gmail.com>
2024-01-23 12:59:05 +0700

    adjust card

--- src/components/LayananCard.jsx
+++ src/components/LayananCard.jsx
@@ -2,17 +2,17 @@
 
 import { Button, Card } from 'flowbite-react';
 
-export default function LayananCard({title}) {
+export default function LayananCard({ title, selengkapnya }) {
   return (
-    <Card className="w-80">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-700 dark:text-white">
+    <Card className="w-[390px] rounded-xl border-black">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-700 dark:text-white text-center">
         {title}
       </h5>
-      <p className="font-normal text-gray-500 dark:text-gray-400">
+      <p className="font-normal text-gray-500 dark:text-gray-400 mb-5">
         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
       </p>
-      <Button color='failure' href='/analisis'>
-        Read more
+      <Button color='failure' href='/analisis' className='bg-gradient-to-r from-red-700 via-red-700 to-rose-950 font-bold py-2 '>
+        {selengkapnya}
         <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path
             fillRule="evenodd"
