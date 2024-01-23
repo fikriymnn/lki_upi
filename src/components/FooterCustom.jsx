27ede0087a9d72312456fffb8efaa70d0f66a2e1
TionoviarId <thenoviar@gmail.com>
2024-01-23 16:53:23 +0700

    change footer

--- src/components/FooterCustom.jsx
+++ src/components/FooterCustom.jsx
@@ -1,58 +1,59 @@
-
-'use client';
-
-import { Footer } from 'flowbite-react';
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
-
-export default function FooterCustom() {
+import React from "react";
+import Image from "next/image";
+function FooterCustom() {
   return (
-    <Footer container>
-      <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <Footer.Brand
-              href="/"
-              src="/logo.svg"
-              alt="Flowbite Logo"
-              name="LKI UPI"
+    <div>
+      <div className="mx-auto grid md:grid-cols-4 grid-cols-1 bg-[#202020] text-white  py-9">
+        <Image
+          alt=""
+          src={"/footer.png"}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-[343px]"
+        />
+        <div className="w-11/12">
+          <h1 className="mb-5 font-bold text-xl">Tentang Kami</h1>
+          <p>
+            Laboratorium Kimia Instrumen Universitas Pendidikan Indonesia Gedung
+            JICA &#40; FPMIPA-A &#41; Lt. 5 Jl. Dr. Setiabudhi No. 229 Bandung
+            40154
+          </p>
+        </div>
+        <div>
+          <h1 className="mb-5 font-bold text-xl">Kontak Kami</h1>
+          <div className="flex gap-3 mb-6">
+            <Image
+              src={"/images/wa.svg"}
+              alt=""
+              width={0}
+              height={0}
+              className="w-6"
             />
+            <p>+61892765628</p>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <Footer.Title title="about" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Flowbite</Footer.Link>
-                <Footer.Link href="#">Tailwind CSS</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Follow us" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Github</Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
+          <div className="flex gap-3">
+            <Image
+              src={"/images/email.svg"}
+              alt=""
+              width={0}
+              height={0}
+              className="w-6"
+            />
+            <p>lkiupi2022@gmail.com</p>
           </div>
         </div>
-        <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+        <div>
+          <h1 className="mb-5 font-bold text-xl">Layanan</h1>
+          <div className="flex flex-col gap-5">
+            <p>Layanan Analisis</p>
+            <p>Sertifikat</p>
+            <p>Pelatihan</p>
           </div>
         </div>
       </div>
-    </Footer>
+    </div>
   );
 }
+
+export default FooterCustom;
