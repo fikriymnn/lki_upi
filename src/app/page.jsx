2b12d8460ca5146900e0d8bc51948cff28df07d0
Tio Noviar <thenoviar@gmail.com>
2024-01-25 16:46:35 +0700

    adjust page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,6 @@ export default function Home() {
     <>
       <CarouselCustom />
       <div className=" ">
-        {/* <p className='text-center mt-14 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda sint qui libero neque natus veritatis delectus in praesentium vero saepe commodi impedit quis aspernatur, similique voluptas totam repellat fugiat ullam!</p> */}
         <div className="">
           <h1 className="text-center text-4xl font-bold mt-10 mb-5 text-gray-800">
             LAYANAN DAN JASA LAB KIMIA UPI
@@ -17,16 +16,17 @@ export default function Home() {
           </div>
         </div>
         <div className="p-16 ">
-          <div className="grid md:grid-cols-3 grid-cols-1 justify-items-center ">
+          <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center gap-6">
             <LayananCard
               title={"Layanan Analisis "}
               selengkapnya={"Selengkapnya"}
+              desc={"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.  biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order"}
             />
-            <LayananCard title={"Sertifikasi"} selengkapnya={"Segera Hadir"} />
-            <LayananCard title={"Pelatihan"} selengkapnya={"Segera Hadir"} />
+            <LayananCard title={"Sertifikasi"} selengkapnya={"Segera Hadir"} desc={"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."} />
+            <LayananCard title={"Pelatihan"} selengkapnya={"Segera Hadir"} desc={"Here are the biggest enterprise technology acquisitions of 2021 so "} />
           </div>
           <div className="flex justify-center mt-[86px]">
-            <div className="px-20 py-4 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+            <div className="px-20 py-4 bg-slate-900 text-white grad rounded-xl font-bold text-lg">
               Pesan Sekarang
             </div>
           </div>
