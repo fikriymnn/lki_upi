ecd4c859d353eabe82bf88c926146ddf73012d1f
Tio Noviar <thenoviar@gmail.com>
2024-01-25 16:47:27 +0700

    adjust page

--- src/components/CarouselCustom.jsx
+++ src/components/CarouselCustom.jsx
@@ -4,88 +4,90 @@ import { Carousel } from "flowbite-react";
 
 export default function CarouselCustom() {
   return (
-    <div className="h-72 sm:h-96 xl:h-96 2xl:h-96">
+    <div className="h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full ">
       <Carousel>
-        <div className="flex ">
-          <img src="./carousel1.jpg" alt="..." className=" w-full " />
-          <div className="flex absolute z-50 w-[700px] flex-col">
-            <p className=" mx-[112px] pt-[200px]  text-white font-bold text-5xl ">
+
+        <div className="flex bg-neutral-900 ">
+          <img src="./carousel1.jpg" alt="..." className="h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full opacity-70" />
+
+          <div className="flex absolute z-40 md:w-[700px] sm:w-[400px] w-[400px] flex-col translate-y-[5vw] sm:translate-y-[8vw] md:translate-y-[10vw] lg:translate-y-[7vw] ">
+            <p className=" md:mx-[112px] sm:mx-[50px] mx-[10px]  text-white font-bold md:text-5xl sm:text-xl text-lg ">
               Laboratorium Kimia Instrumen UPI
             </p>
-            <p className="font-semibold text-xl mx-[112px] ">
+            <p className="font-semibold md:text-xl sm:text-lg text-sm md:mx-[112px] sm:mx-[50px] mx-[10px] text-white ">
               Laboratorium Kimia Instrumen UPI merupakan bla bla bla bla dengan
               bla bla bla bla bla.
             </p>
             <div className="flex justify-center mt-5">
-              <div className="px-10 py-2 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+              <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
                 Tentang Kami
               </div>
             </div>
           </div>
         </div>
-        <div className="flex ">
-          <img src="./carousel1.jpg" alt="..." className=" w-full " />
-          <div className="flex absolute z-50 w-[700px] flex-col">
-            <p className=" mx-[112px] pt-[200px]  text-white font-bold text-5xl ">
+        <div className="flex bg-neutral-900">
+          <img src="./carousel1.jpg" alt="..." className="h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full opacity-70" />
+          <div className="flex absolute z-50 md:w-[700px] sm:w-[400px] w-[400px] flex-col translate-y-[5vw] sm:translate-y-[8vw] md:translate-y-[10vw] lg:translate-y-[7vw]">
+            <p className=" md:mx-[112px] sm:mx-[50px] mx-[10px]  text-white font-bold md:text-5xl sm:text-xl text-lg ">
               Laboratorium Kimia Instrumen UPI
             </p>
-            <p className="font-semibold text-xl mx-[112px] ">
+            <p className="font-semibold md:text-xl sm:text-lg text-sm md:mx-[112px] sm:mx-[50px] mx-[10px] text-white ">
               Laboratorium Kimia Instrumen UPI merupakan bla bla bla bla dengan
               bla bla bla bla bla.
             </p>
             <div className="flex justify-center mt-5">
-              <div className="px-10 py-2 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+              <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
                 Tentang Kami
               </div>
             </div>
           </div>
         </div>
-        <div className="flex ">
-          <img src="./carousel.jpg" alt="..." className=" w-full " />
-          <div className="flex absolute z-50 w-[700px] flex-col">
-            <p className=" mx-[112px] pt-[200px]  text-white font-bold text-5xl ">
+        <div className="flex bg-neutral-900">
+          <img src="./carousel.jpg" alt="..." className=" h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full opacity-70" />
+          <div className="flex absolute z-50 md:w-[700px] sm:w-[400px] w-[400px] flex-col translate-y-[5vw] sm:translate-y-[8vw] md:translate-y-[10vw] lg:translate-y-[7vw]">
+            <p className=" md:mx-[112px] sm:mx-[50px] mx-[10px]  text-white font-bold md:text-5xl sm:text-xl text-lg ">
               Laboratorium Kimia Instrumen UPI
             </p>
-            <p className="font-semibold text-xl mx-[112px] ">
+            <p className="font-semibold md:text-xl sm:text-lg text-sm md:mx-[112px] sm:mx-[50px] mx-[10px] text-white ">
               Laboratorium Kimia Instrumen UPI merupakan bla bla bla bla dengan
               bla bla bla bla bla.
             </p>
             <div className="flex justify-center mt-5">
-              <div className="px-10 py-2 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+              <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
                 Tentang Kami
               </div>
             </div>
           </div>
         </div>
-        <div className="flex ">
-          <img src="./carousel1.jpg" alt="..." className=" w-full " />
-          <div className="flex absolute z-50 w-[700px] flex-col">
-            <p className=" mx-[112px] pt-[200px]  text-white font-bold text-5xl ">
+        <div className="flex bg-neutral-900">
+          <img src="./carousel1.jpg" alt="..." className="h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full opacity-70" />
+          <div className="flex absolute z-50 md:w-[700px] sm:w-[400px] w-[400px] flex-col translate-y-[5vw] sm:translate-y-[8vw] md:translate-y-[10vw] lg:translate-y-[7vw]">
+            <p className=" md:mx-[112px] sm:mx-[50px] mx-[10px]  text-white font-bold md:text-5xl sm:text-xl text-lg ">
               Laboratorium Kimia Instrumen UPI
             </p>
-            <p className="font-semibold text-xl mx-[112px] ">
+            <p className="font-semibold md:text-xl sm:text-lg text-sm md:mx-[112px] sm:mx-[50px] mx-[10px] text-white ">
               Laboratorium Kimia Instrumen UPI merupakan bla bla bla bla dengan
               bla bla bla bla bla.
             </p>
             <div className="flex justify-center mt-5">
-              <div className="px-10 py-2 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+              <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
                 Tentang Kami
               </div>
             </div>
           </div>
         </div>
-        <div className="flex ">
-          <img src="./carousel.jpg" alt="..." className=" w-full " />
-          <div className="flex absolute z-50 w-[700px] flex-col">
-            <p className=" mx-[112px] pt-[200px]  text-white font-bold text-5xl ">
+        <div className="flex bg-neutral-900">
+          <img src="./carousel.jpg" alt="..." className="h-48 sm:h-80 xl:h-[400px] 2xl:h-[450px] w-full opacity-70" />
+          <div className="flex absolute z-50 md:w-[700px] sm:w-[400px] w-[400px] flex-col translate-y-[5vw] sm:translate-y-[8vw] md:translate-y-[10vw] lg:translate-y-[7vw]">
+            <p className=" md:mx-[112px] sm:mx-[50px] mx-[10px]  text-white font-bold md:text-5xl sm:text-xl text-lg ">
               Laboratorium Kimia Instrumen UPI
             </p>
-            <p className="font-semibold text-xl mx-[112px] ">
+            <p className="font-semibold md:text-xl sm:text-lg text-sm md:mx-[112px] sm:mx-[50px] mx-[10px] text-white ">
               Laboratorium Kimia Instrumen UPI merupakan bla bla bla bla dengan
               bla bla bla bla bla.
             </p>
             <div className="flex justify-center mt-5">
-              <div className="px-10 py-2 bg-slate-900 text-white bg-gradient-to-r from-red-700 via-red-700 to-rose-950 rounded-xl font-bold text-lg">
+              <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
                 Tentang Kami
               </div>
             </div>
