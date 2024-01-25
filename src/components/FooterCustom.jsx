6eab34c694f73dc2a004875b52d17dc433657ac9
Tio Noviar <thenoviar@gmail.com>
2024-01-25 16:47:20 +0700

    adjust page

--- src/components/FooterCustom.jsx
+++ src/components/FooterCustom.jsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 function FooterCustom() {
   return (
     <div>
-      <div className="mx-auto grid md:grid-cols-4 grid-cols-1 bg-[#202020] text-white  py-9">
+      <div className="mx-auto grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-5 bg-[#202020] text-white  py-9 ">
         <Image
           alt=""
           src={"/footer.png"}
           width={0}
           height={0}
           sizes="100vw"
-          className="w-[343px]"
+          className="md:w-[343px] w-[250px] md:mx-0 mx-3"
         />
-        <div className="w-11/12">
-          <h1 className="mb-5 font-bold text-xl">Tentang Kami</h1>
+        <div className="w-11/12 md:mx-0 mx-3">
+          <h1 className="mb-5 font-bold md:text-xl text-lg">Tentang Kami</h1>
           <p>
             Laboratorium Kimia Instrumen Universitas Pendidikan Indonesia Gedung
             JICA &#40; FPMIPA-A &#41; Lt. 5 Jl. Dr. Setiabudhi No. 229 Bandung
             40154
           </p>
         </div>
-        <div>
-          <h1 className="mb-5 font-bold text-xl">Kontak Kami</h1>
+        <div className="md:mx-0 mx-3">
+          <h1 className="mb-5 font-bold md:text-xl text-lg">Kontak Kami</h1>
           <div className="flex gap-3 mb-6">
             <Image
               src={"/images/wa.svg"}
@@ -43,9 +43,9 @@ function FooterCustom() {
             <p>lkiupi2022@gmail.com</p>
           </div>
         </div>
-        <div>
-          <h1 className="mb-5 font-bold text-xl">Layanan</h1>
-          <div className="flex flex-col gap-5">
+        <div className="md:mx-0 mx-3">
+          <h1 className="mb-5 font-bold md:text-xl text-lg">Layanan</h1>
+          <div className="flex flex-col md:gap-5 gap-2">
             <p>Layanan Analisis</p>
             <p>Sertifikat</p>
             <p>Pelatihan</p>
