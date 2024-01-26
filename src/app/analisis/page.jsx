2d5dd2f17f09e66609db56cde10e4ca3ecd465fd
Tio Noviar <thenoviar@gmail.com>
2024-01-26 17:09:06 +0700

    adjust page

--- src/app/analisis/page.jsx
+++ src/app/analisis/page.jsx
@@ -10,44 +10,47 @@ export default function analisis() {
 
     return (
         <>
-            <div className=" flex md:h-[229px] h-40 w-full  ">
-                <Image
-                    alt=""
-                    src={"/carousel.jpg"}
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className=" md:h-[229px] h-40 w-full"
-                />
-                <div className="bg-neutral-900 bg-opacity-70 w-full md:h-[229px] h-40  absolute flex flex-col justify-center items-center">
-                    <p className="text-white md:text-5xl sm:text-xl text-lg font-bold">
-                        Layanan Jasa Analisis LKI UPI
-                    </p>
-                    <div className="flex justify-center mt-5">
-                        <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
-                            Tentang Kami
+            <main className=''>
+
+                <div className=" flex md:h-[229px] h-40 w-full  ">
+                    <Image
+                        alt=""
+                        src={"/carousel.jpg"}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className=" md:h-[229px] h-40 w-full"
+                    />
+                    <div className="bg-neutral-900 bg-opacity-70 w-full md:h-[229px] h-40  absolute flex flex-col justify-center items-center">
+                        <p className="text-white md:text-5xl sm:text-xl text-lg font-bold">
+                            Layanan Jasa Analisis LKI UPI
+                        </p>
+                        <div className="flex justify-center mt-5">
+                            <div className="px-10 py-2 bg-slate-900 text-white grad rounded-xl font-bold md:text-lg sm:text-base text-sm">
+                                Tentang Kami
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=''>
-                <p className='text-center text-4xl font-bold text-gray-800 mt-7'>Layanan Pengujian Laboratorium Kimia Instrumen LKI UPI</p>
-                <div className='flex justify-center'>
-                    <hr className='text-red-700 bg-red-600 h-2 mb-8 mt-5 w-56 text-center' />
-                </div>
-                <div className='grid grid-cols-3'>
-                    <CardPenguji />
-                    <CardPenguji />
-                    <CardPenguji />
-                </div>
+                <div className='mx-[60px]'>
+                    <p className='text-center text-4xl font-bold text-gray-800 mt-7'>Layanan Pengujian Laboratorium Kimia Instrumen LKI UPI</p>
+                    <div className='flex justify-center'>
+                        <hr className='text-red-700 bg-red-600 h-2 mb-8 mt-5 w-56 text-center' />
+                    </div>
+                    <div className='grid grid-cols-3 gap-10'>
+                        <CardPenguji />
+                        <CardPenguji />
+                        <CardPenguji />
+                    </div>
 
-                <div className='flex justify-center mt-24'>
-                    <ButtonOrder className="" />
-                </div>
-                <br />
-                <br />
+                    <div className='flex justify-center mt-24'>
+                        <ButtonOrder className="" />
+                    </div>
+                    <br />
+                    <br />
 
-            </div>
+                </div>
+            </main>
         </>
     )
 }
