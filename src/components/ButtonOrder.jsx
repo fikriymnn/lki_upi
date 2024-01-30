26b18c256c570b531c604a2764d501f46eba7e23
Tio Noviar <thenoviar@gmail.com>
2024-01-30 15:20:52 +0700

    add onclick

--- src/components/ButtonOrder.jsx
+++ src/components/ButtonOrder.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import { UserContext } from '@/context/userContext'
 import { useState, useContext } from 'react'
-import { useRouter, usePathname} from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import axios from 'axios'
 
 
@@ -31,27 +31,30 @@ export default function ButtonOrder() {
     //   },[])
 
     async function pesanLayanan() {
-        try{
-            const data = await axios("http://localhost:5000/api/user",{ withCredentials: true })
+        try {
+            const data = await axios("http://localhost:5000/api/user", { withCredentials: true })
             console.log(data)
             if (data.data.success == true) {
-                if(data.data.data.role=="user"){
+                if (data.data.data.role == "user") {
                     router.push(`/order_analisis`)
                 }
-            }else{
+            } else {
                 router.push(`/login?prevRoute=${prevRoute}`)
             }
-          }catch(err){
-               router.push(`/login?prevRoute=${prevRoute}`)
-          }
-    }  
+        } catch (err) {
+            router.push(`/login?prevRoute=${prevRoute}`)
+        }
+    }
 
 
     return (
         <>
             <div className='cursor-pointer'>
-                <div className='w-40 h-full bg-red-700 rounded-lg' onClick={() => pesanLayanan()}>
-                    <h1 className='px-5 py-3 font-semibold text-white'>Pesan Layanan</h1>
+
+                <div className="flex justify-center mt-[86px]">
+                    <div className="px-20 py-4 bg-slate-900 text-white grad rounded-xl font-bold text-lg" onClick={() => pesanLayanan()}>
+                        Pesan Sekarang
+                    </div>
                 </div>
             </div>
         </>
