961b18aa8ab0ec2519a601d44764290beebdccf9
Luthfikhaerii <luthfikhaeri00@gmail.com>
2024-02-11 15:08:41 +0700

    update

--- src/app/admin/dashboard/operator/detail/[id]/page.jsx
+++ src/app/admin/dashboard/operator/detail/[id]/page.jsx
@@ -38,14 +38,14 @@ export default function detailOrderOperator({ params, searchParams }) {
             <div className='flex justify-center'>
                 <hr className='text-red-700 bg-red-600 h-2 mb-8 mt-5 w-56 text-center' />
             </div>
-            <div className="mx-20">
+            <div className="md:mx-20 mx-5">
             
             <div className="grid grid-cols-2  border-2 rounded-lg p-2 border-b-2"><p className="md:text-xl sm:text-xl text-lg font-semibold ">No Invoice : </p> <p className="ml-3 font-semibold text-gray-600 md:text-base sm:text-sm text-xs">{invoice.no_invoice ? invoice.no_invoice : ""}</p></div>
             <div className="grid grid-cols-2  border-2 rounded-lg p-2 border-b-2"><p className="md:text-xl sm:text-xl text-lg font-semibold ">Status : </p> <p className="ml-3 font-semibold text-gray-600 md:text-base sm:text-sm text-xs">{invoice.status ? invoice.status : ""}</p></div>
             <div className="grid grid-cols-2  border-2 rounded-lg p-2 border-b-2"><p className="md:text-xl sm:text-xl text-lg font-semibold ">Estimasi selesai : </p> <p className="ml-3 font-semibold text-gray-600 md:text-base sm:text-sm text-xs">{invoice.estimasi_date ? invoice.estimasi_date : ""}</p></div>
             </div>
             <br />
-            <div className="mx-20">
+            <div className="md:mx-20 mx-5">
                 {
                     order.map((e, i) => {
                         return (<AdminOrderCard riwayat_pengujian={e.riwayat_pengujian} sample_dikembalikan={e.sample_dikembalikan} uuid={e.uuid} id={e._id} jenis_pengujian={e.jenis_pengujian} kode_pengujian={e.kode_pengujian} nama_sample={e.nama_sample} jumlah_sample={e.jumlah_sample} index={i + 1} wujud_sample={e.wujud_sample} pelarut={e.pelarut} preparasi_khusus={e.preparasi_khusus} target_senyawa={e.target_senyawa} metode_parameter={e.metode_parameter} jurnal_pendukung={e.jurnal_pendukung} foto_sample={e.foto_sample} deskripsi={e.deskripsi_sample} hasil_analisis={e.hasil_analisis} key={i} />)
