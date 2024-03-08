899fd354927d2d7c929271d88ac5ccdb74a03625
Luthfikhaerii <luthfikhaeri00@gmail.com>
2024-03-08 17:48:58 +0700

    upda

--- src/components/Navigasi.jsx
+++ src/components/Navigasi.jsx
@@ -5,7 +5,7 @@ import { Breadcrumb } from 'flowbite-react';
 
 export default function Navigasi({text1,text2}) {
   return (
-    <Breadcrumb aria-label="Default breadcrumb example" className='md:mt-8 md:ml-20 mt-8 ml-10'>
+    <Breadcrumb aria-label="Default breadcrumb example" className='md:mt-8 md:ml-14 mt-8 ml-10'>
       <Breadcrumb.Item ><p className='md:text-xl text-lg'>{text1}</p></Breadcrumb.Item>
       <Breadcrumb.Item ><p className='md:text-xl text-lg'>{text2}</p></Breadcrumb.Item>
     </Breadcrumb>
