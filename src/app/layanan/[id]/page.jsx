fa5fa6eb9a511750c4a9ee87f5bf166d1954d48f
Tio Noviar <135575246+TionoviarId@users.noreply.github.com>
2024-03-23 11:41:26 +0700

    s

--- src/app/layanan/[id]/page.jsx
+++ src/app/layanan/[id]/page.jsx
@@ -57,7 +57,7 @@ function Page({ params }) {
                             </p>
                             </div>
                         </div>
-                        <div className='md:w-8/12 sm:w-8/12 flex items-center'>
+                        <div className='md:w-8/12 sm:w-6/12 flex items-center'>
                             <div className='md:text-[18px] sm:text-[10px] text-[9px] font-medium mt-5 md:mt-0 sm:mt-5'>
                                 {parse(data?.deskripsi)}
                             </div>
