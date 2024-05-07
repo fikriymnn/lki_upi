38552400318b62d9de5d38fe885908d84e5c1971
Fikri Zaki Aditama, S.Si <fikriyomann@student.upi.edu>
2024-05-08 04:26:45 +0700

    coba SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://lablkiupi.com";
+  return {
+    rules: {
+      userAgent: "*",
+      allow: ["/", "/about", "/contact", "layanan", "/analisis"],
+      disallow: [
+        "/admin",
+        "/login?prevRoute=/layanan",
+        "/admin/dashboard/admin",
+        "/admin/dashboard/admin/order",
+        "/admin/dashboard/admin/history_order",
+        "/admin/dashboard/admin/user",
+        "/admin/dashboard/admin/report",
+        "/admin/dashboard/operator",
+        "/admin/dashboard/pj",
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
