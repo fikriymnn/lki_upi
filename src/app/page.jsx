f10209306f90fb1a2b088d5b5461455f5c1eb588
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-06-07 10:41:51 +0700

    tampilan font

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,21 +2,24 @@ import Image from "next/image";
 import CarouselCustom from "@/components/CarouselCustom";
 import LayananCard from "@/components/LayananCard";
 import ButtonOrder from "@/components/ButtonOrder";
+import { Noto_Sans } from 'next/font/google';
+
+const inter = Noto_Sans({ subsets: ['latin'],weight: "500" });
 
 export default function Home() {
   return (
     <>
       <CarouselCustom />
       <div className=" ">
-        <div className="">
-          <h1 className="text-center md:text-5xl m-auto sm:text-3xl text-2xl font-bold w-10/12 md:mt-14 sm:mt-6 mt-10 mb-5 text-gray-800">
-            LAYANAN DAN JASA LAB KIMIA UPI
+        <div className={inter.className}>
+          <h1 className="text-center md:text-4xl m-auto sm:text-3xl text-2xl font-bold w-10/12 md:mt-14 sm:mt-6 mt-10 mb-2 text-gray-800">
+            Layanan dan Jasa Lab Kimia UPI
           </h1>
           <div className="flex justify-center">
-            <hr className="text-red-700 bg-red-600  h-2 md:mb-14 sm:mb-5 mb-5 w-8/12 text-center" />
+            <hr className="text-red-700 bg-red-600  h-[5px] md:mb-14 sm:mb-5 mb-5 w-7/12 text-center" />
           </div>
         </div>
-        <div className="md:px-16 sm:px-16 px-8">
+        <div className="md:px-16 sm:px-16 px-10">
           <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center gap-6">
             <LayananCard
               title={"Layanan Analisis "}
