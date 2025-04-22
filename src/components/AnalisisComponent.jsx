df933f38e5f7a964fe31442330541cfd72e04d86
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-04-22 16:27:51 +0700

    update posisi tampilan

--- src/components/AnalisisComponent.jsx
+++ src/components/AnalisisComponent.jsx
@@ -29,14 +29,13 @@ export default function AnalisisComponent() {
 
   return (
     <>
+      
       <p className="text-center md:text-4xl sm:text-2xl text-xl font-bold text-gray-800 mt-7">
-        Layanan Pengujian Laboratorium Kimia Instrumen LKI UPI
+        Daftar Alat Laboratorium Kimia Instrumen UPI
       </p>
       <div className="flex justify-center">
         <hr className="grad h-2 mb-8 mt-5 w-56 text-center" />
       </div>
-      <ButtonOrder />
-      <br />
       <div className="grid md:grid-cols-3 grid-cols-1 md:gap-10 gap-0">
         {data.map((v, i) => (
           <>
@@ -45,6 +44,14 @@ export default function AnalisisComponent() {
         ))}
       </div>
 
+      <br />
+      <p className="text-center md:text-4xl sm:text-2xl text-xl font-bold text-gray-800 mt-7">
+        Layanan Pengujian Laboratorium Kimia Instrumen LKI UPI
+      </p>
+      <div className="flex justify-center">
+        <hr className="grad h-2 mb-8 mt-5 w-56 text-center" />
+      </div>
+      <ButtonOrder />
       <br />
     </>
   );
